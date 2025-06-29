aa09edb0a595c4d665659a4e6eb85a8cc88bda53
Waqas Ilyas <wilyas@ritchiebros.com>
2025-06-29 16:06:23 +0300

    fix: update user details in Pact test to reflect correct age attribute

--- src/pact/user.pact.test.js
+++ src/pact/user.pact.test.js
@@ -28,7 +28,7 @@ describe('Pact with User Service', () => {
                 willRespondWith: {
                     status: 200,
                     headers: { 'Content-Type': 'application/json' },
-                    body: { id: 1, name: 'John Doe' },
+                    body: { id: 1, age: "30" },
                 },
             });
         });
@@ -38,7 +38,7 @@ describe('Pact with User Service', () => {
             const data = await response.json();
 
             expect(response.status).toEqual(200);
-            expect(data).toEqual({ id: 1, name: 'John Doe' });
+            expect(data).toEqual({ id: 1, age: "30" });
         });
     });
 });
